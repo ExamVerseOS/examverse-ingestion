5c976e750e80c836b9cc5ed48fe580ef790ca687
KnowVerseOS <makashbsp1997@gmail.com>
2026-06-28 14:22:03 +0530

    Create syllabus_urls.py

---
+++ app/sources/upsc/syllabus_urls.py
@@ -0,0 +1,5 @@
+UPSC_SYLLABUS_URLS = {
+    "prelims": "https://upsc.gov.in/syllabus-general-studies-paper-i",
+    "mains_gs1": "https://upsc.gov.in/syllabus-mains-general-studies-paper-1",
+    "mains_gs2": "https://upsc.gov.in/syllabus-mains-general-studies-paper-2",
+}
